5725dd280d950c6515f4d4817ead07660e8f0346
WJ-Lai <418320384@qq.com>
2019-11-19 20:42:41 +0800

    21

---
+++ 1. 链表/21.合并两个有序链表.py
@@ -0,0 +1,35 @@
+# 将两个有序链表合并为一个新的有序链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。 
+
+# 示例：
+# 输入：1->2->4, 1->3->4
+# 输出：1->1->2->3->4->4
+
+# Definition for singly-linked list.
+# class ListNode(object):
+    # def __init__(self, x, next=None):
+        # self.val = x
+        # self.next = next
+
+class Solution(object):
+    def mergeTwoLists(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        if not l1:
+            return l2
+        elif not l2:
+            return l1
+        elif l1.val < l2.val:
+            l1.next = self.mergeTwoLists(l1.next, l2)
+            return l1
+        else:
+            l2.next = self.mergeTwoLists(l1, l2.next)
+            return l2
+
+# l1=ListNode(1,next=ListNode(2,next=ListNode(3)))
+# l2=ListNode(4,next=ListNode(5,next=ListNode(6)))
+# s = Solution()
+# head = s.mergeTwoLists(l1, l2)
+# head
