ef2d31f9bb450abdb4af23aaac66fbaf9b12819b
WJ-Lai <418320384@qq.com>
2019-11-19 21:04:10 +0800

    21

--- 1. 链表/21.合并两个有序链表.py
+++ 1. 链表/21.合并两个有序链表.py
@@ -6,12 +6,13 @@
 
 # Definition for singly-linked list.
 # class ListNode(object):
-    # def __init__(self, x, next=None):
-        # self.val = x
-        # self.next = next
+#     def __init__(self, x, next=None):
+#         self.val = x
+#         self.next = next
 
 class Solution(object):
-    def mergeTwoLists(self, l1, l2):
+    # 递归法
+    def mergeTwoLists1(self, l1, l2):
         """
         :type l1: ListNode
         :type l2: ListNode
@@ -28,8 +29,29 @@ class Solution(object):
             l2.next = self.mergeTwoLists(l1, l2.next)
             return l2
 
+    # 迭代法
+    def mergeTwoLists2(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        prehead = ListNode(-1)
+        prev = prehead
+
+        while l1 and l2:
+            if l1.val < l2.val:
+                prev.next, l1 = l1, l1.next
+            else:
+                prev.next, l2 = l2, l2.next
+            prev = prev.next
+
+        prev.next = l1 if l1 is not None else l2
+
+        return prehead.next
+
 # l1=ListNode(1,next=ListNode(2,next=ListNode(3)))
 # l2=ListNode(4,next=ListNode(5,next=ListNode(6)))
 # s = Solution()
 # head = s.mergeTwoLists(l1, l2)
-# head
+# print(head)
