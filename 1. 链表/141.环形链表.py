b0748fc37b6a203a61113e03c636c031f0d78cda
WJ-Lai <418320384@qq.com>
2019-11-20 10:00:29 +0800

    141

--- 1. 链表/141.环形链表.py
+++ 1. 链表/141.环形链表.py
@@ -5,7 +5,13 @@
 #         self.next = None
 
 class Solution(object):
-    def hasCycle1(self, head):
+
+    # 其他方法
+    # 修改数据法，一个指针遍历，每到一个地方val改为None
+    # 限时法/限数法，限制遍历指针的时间/数量，超过阈值则认为有环
+
+    # 双指针法，时间复杂度O(N)，空间复杂度O(N)
+    def hasCycle(self, head):
         """
         :type head: ListNode
         :rtype: bool
@@ -23,3 +29,22 @@ class Solution(object):
             else:
                 l_fast = l_fast.next
         return False
+
+    # 哈希表法，时间复杂度O(N)，空间复杂度O(1)
+    def hasCycle2(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        if not head or not head.next: return False
+
+        list_node = []
+
+        while head:
+            if head in list_node:
+                return True
+            else:
+                list_node.append(head)
+            head = head.next
+        return False
+
